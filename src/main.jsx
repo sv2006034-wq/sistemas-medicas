618d2b836560be933bc032dd90c8e0bab623094d
Samuel Vasquez <sv2006034@gmail.com>
2025-11-21 21:47:58 -0500

    Quitar basename para Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css' // Aqui es donde se importa el CSS
+import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/sistemas-medicas">
-    <App />
+    <BrowserRouter>
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
+
+
